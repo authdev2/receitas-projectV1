b42377db0b58f4c7e1bd3ead91ba93268ddf2b0b
authdev2 <flaviogomes05.work@gmail.com>
2025-08-14 16:39:24 +0100

    Novas funcionalidades por acabar

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const apiKey = "1cb880b0e01b44c0a9f8799e352301e27";
+const apiKey = "2eabe7358cbf40fb9b8fbbf1457849c6";
 
 let receita = document.querySelector(".receita-destaque-info");
 let receitaRight = document.querySelector(".receita-right");
-fetch(`https://api.spoonacular.com/recipes/random?number=1&apiKey=${apiKey}`)
+fetch(`h1ttps://api.spoonacular.com/recipes/random?number=1&apiKey=${apiKey}`)
   .then((response) => response.json())
   .then((data) => {
     data.recipes.forEach((recipe) => {
@@ -49,3 +49,80 @@ fetch(`https://api.spoonacular.com/recipes/random?number=1&apiKey=${apiKey}`)
   .catch((error) => {
     console.error("Erro ao buscar receitas:", error.message);
   });
+
+  let btnReceitaSearch = document.querySelector(".btn-receita-search");
+  let inputReceitaSearch = document.querySelector(".input-receita-search");
+  btnReceitaSearch.addEventListener("click", () => {
+    SearchReceitas(inputReceitaSearch.value);
+  })
+
+  function SearchReceitas(query){
+    let boxReceitaInfo = document.querySelector(".receita-content");
+    boxReceitaInfo.innerHTML = "";
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((recipe) => {
+        let div = document.createElement("div");
+        div.classList.add("box-receita");
+        div.innerHTML = `
+          <h1>${recipe.title}</h1>
+          <div class="box-receita-img">
+            <img src="${recipe.image}" alt="">
+          </div>
+          <div class="box-receita-info">
+           <span>
+            ${recipe.readyInMinutes} minutos
+           </span>
+           <span>
+            ${recipe.servings} porções
+           </span>
+           <span>
+            ${recipe.healthScore}% saudável
+           </span>
+           <button class="btn-receita">Ver receita</button>
+          </div>
+        `;
+        boxReceitaInfo.appendChild(div);
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar receitas:", error.message);
+    });
+  }
+
+  function RandomReceitas(){
+    let boxReceitaInfo = document.querySelector(".receita-content");
+    fetch(`1https://api.spoonacular.com/recipes/random?number=1&apiKey=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.recipes.forEach((recipe) => {
+        let div = document.createElement("div");
+        div.classList.add("box-receita");
+        div.innerHTML = `
+          <h1>${recipe.title}</h1>
+          <div class="box-receita-img">
+            <img src="${recipe.image}" alt="">
+          </div>
+          <div class="box-receita-info">
+           <span>
+            ${recipe.readyInMinutes} minutos
+           </span>
+           <span>
+            ${recipe.servings} porções
+           </span>
+           <span>
+            ${recipe.healthScore}% saudável
+           </span>
+           <button class="btn-receita">Ver receita</button>
+          </div>
+        `;
+        boxReceitaInfo.appendChild(div);
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar receitas:", error.message);
+    });
+  }
+
+  RandomReceitas();
